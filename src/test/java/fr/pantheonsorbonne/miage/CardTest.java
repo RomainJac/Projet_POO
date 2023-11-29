61dfd16c7275209f0582f88a22fe2533932891c9
Romain <romainjacob012@gmail.com>
2023-11-29 10:34:00 +0100

    Debut Test

---
+++ src/test/java/fr/pantheonsorbonne/miage/CardTest.java
@@ -0,0 +1,30 @@
+package fr.pantheonsorbonne.miage;
+
+import org.junit.jupiter.api.Test;
+
+import fr.pantheonsorbonne.miage.game.classes.Card;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CardTest {
+
+    @Test
+    public void testCardConstructor() {
+        Card card = new Card(Card.cardRank.AS, Card.cardColor.CARREAU);
+        assertEquals(Card.cardRank.AS, card.getCardRank());
+        assertEquals(Card.cardColor.CARREAU, card.getCardColor());
+    }
+
+    @Test
+    public void testCardToString() {
+        Card card = new Card(Card.cardRank.AS, Card.cardColor.CARREAU);
+        assertEquals("AS de CARREAU", card.toString());
+    }
+
+    @Test
+    public void testCardGetCardName() {
+        Card card = new Card(Card.cardRank.AS, Card.cardColor.CARREAU);
+        assertEquals("AS de CARREAU", card.getCardName());
+    }
+
+}
